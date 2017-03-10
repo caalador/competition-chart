6ad8e56a2d56bc3f13344ebe1b064447a44f3284
Mikael Grankvist <mikael.grankvist@vaadin.com>
2017-03-10 15:06:55 +0200

    Update to Vaadin 8.0.0

---
+++ Competition-chart-addon/src/main/java/com/competition/chart/visualisation/client/ui/CKnockoutChart.java
@@ -0,0 +1,711 @@
+package com.competition.chart.visualisation.client.ui;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
+import com.competition.chart.visualisation.client.Competitor;
+import com.competition.chart.visualisation.client.Group;
+import com.google.gwt.canvas.client.Canvas;
+import com.google.gwt.dom.client.Document;
+import com.google.gwt.dom.client.Touch;
+import com.google.gwt.event.dom.client.TouchEvent;
+import com.google.gwt.event.shared.HandlerRegistration;
+import com.google.gwt.user.client.DOM;
+import com.google.gwt.user.client.Event;
+import com.google.gwt.user.client.Window;
+import com.google.gwt.user.client.ui.Widget;
+import com.vaadin.client.VConsole;
+
+public class CKnockoutChart extends Widget {
+
+	/** Set the CSS class name to allow styling. */
+	public static final String CLASSNAME = "v-sportchart";
+
+	/** The client side widget identifier */
+	protected String paintableId;
+
+	private final Integer pixelWidth = null;
+
+	private final Canvas canvas;
+
+	private final CPaintGroup paint;
+
+	private final List<Group> groups = new LinkedList<Group>();
+	private Group finalBout, winner;
+	private int onLeft, onRight;
+
+	private int offsetLeft = 15;
+
+	private final List<Group> drawnGroups = new LinkedList<Group>();
+	private boolean onlyLeft = false;
+
+	public static int BOX_WIDTH = 150;
+
+	private boolean mouseDown = false;
+	private boolean mouseMoved = false;
+	private boolean enableDragging = true;
+
+	private final int width = 0;
+	private final int height = 0;
+
+	/**
+	 * The constructor should first call super() to initialize the component and
+	 * then handle any initialization relevant to Vaadin.
+	 */
+	public CKnockoutChart() {
+		setElement(Document.get().createDivElement());
+		setWidth("100%");
+		setHeight("100%");
+
+		setStyleName(CLASSNAME);
+
+		canvas = Canvas.createIfSupported();
+		if (canvas != null) {
+			getElement().appendChild(canvas.getElement());
+			canvas.setVisible(true);
+			canvas.setCoordinateSpaceWidth(50);
+			canvas.setCoordinateSpaceHeight(50);
+		} else {
+			getElement().setInnerHTML("Canvas not supported");
+		}
+
+		if (TouchEvent.isSupported()) {
+			sinkEvents(Event.TOUCHEVENTS);
+		}
+		sinkEvents(Event.MOUSEEVENTS);
+
+		paint = new CPaintGroup(canvas);
+	}
+
+	@Override
+	protected void onAttach() {
+		super.onAttach();
+
+		canvas.setCoordinateSpaceWidth(getElement().getOffsetWidth());
+		canvas.setCoordinateSpaceHeight(getElement().getOffsetHeight());
+	}
+
+	public void resetPositions() {
+		groups.clear();
+	}
+
+	public void allOnLeft(final boolean allOnLeft) {
+		if (allOnLeft) {
+			if (!onlyLeft) {
+				groups.clear();
+			}
+			onlyLeft = true;
+		} else {
+			if (onlyLeft) {
+				groups.clear();
+			}
+			onlyLeft = false;
+		}
+	}
+
+	public void setEnableDragging(final boolean enableDragging) {
+		this.enableDragging = enableDragging;
+	}
+
+	public static void setBoxWidth(final int boxWidth) {
+		BOX_WIDTH = boxWidth;
+	}
+
+	public void checkGroups(final Map<String, List<Competitor>> competitorsByGroup) {
+		boolean newData = false;
+		for (final Group group : groups) {
+			if (!competitorsByGroup.keySet().contains(group.getId()) && !newData) {
+				newData = true;
+				break;
+			}
+		}
+		if (groups.isEmpty()) {
+			newData = true;
+		}
+		if (newData) {
+			groups.clear();
+		}
+
+		for (final String req2 : competitorsByGroup.keySet()) {
+			final Group group = getGroup(req2);
+
+			final List<Competitor> persons = competitorsByGroup.get(req2);
+
+			for (final Competitor person : persons) {
+				group.addName(person);
+			}
+		}
+		Collections.sort(groups, new Comparator<Group>() {
+			@Override
+			public int compare(final Group o1, final Group o2) {
+				return o1.getNumber() == o2.getNumber() ? 0 : o1.getNumber() < o2.getNumber() ? -1 : 1;
+			}
+		});
+
+		buildGroups(newData);
+	}
+
+	private void buildGroups(final boolean newData) {
+		if (!groups.isEmpty()) {
+			if (newData) {
+				offsetLeft = 15;
+			} else {
+				offsetLeft = (int) groups.get(0).getLeftSide();
+			}
+			if (onlyLeft) {
+				winner = null;
+				positionsLeft(newData);
+				buildChildGroupsFromLeft();
+				calculateChildPositions();
+				drawLeftChart();
+			} else {
+				groupPositions(newData);
+				buildChildGroups();
+				calculateChildPositions();
+				drawChart();
+			}
+		}
+	}
+
+	/**
+	 * Get group if exists or create new one if one doesn't
+	 * 
+	 * @param id
+	 * @return
+	 */
+	private Group getGroup(final String id) {
+		for (final Group g : groups) {
+			if (g.getId().equals(id)) {
+				return g;
+			}
+		}
+
+		final Group group = new Group(id);
+		groups.add(group);
+
+		return group;
+	}
+
+	private void groupPositions(final boolean calculateTop) {
+		float top = 35;
+
+		onLeft = groups.size() / 2 + groups.size() % 2;
+		onRight = groups.size() - onLeft;
+
+		for (int i = 0; i < groups.size(); i++) {
+			final Group group = groups.get(i);
+			if (i == onLeft) {
+				if (onLeft > onRight) {
+					top = 25 + getLeftRightDifference();
+					if (onLeft % 2 == 1) {
+						final int names = groups.get(onLeft - 1).getNames().size();
+						if (names > 2) {
+							top -= (names - 2) * 15;
+						} else if (names == 1) {
+							top += 15;
+						}
+					}
+				} else {
+					top = 35;
+				}
+			}
+			if (calculateTop) {
+				top = group.calculatePosition(top);
+			}
+			group.setLeftSide(offsetLeft);
+		}
+	}
+
+	// gets the difference between left and right side so that we get the right
+	// side top offset to match left side center
+	private int getLeftRightDifference() {
+		int left = 0;
+		int right = 0;
+		for (int i = 0; i < groups.size(); i++) {
+			if (i < onLeft) {
+				left++;
+				left += groups.get(i).getNames().size();
+			} else {
+				right++;
+				right += groups.get(i).getNames().size();
+			}
+		}
+		return 20 * (left - right);
+	}
+
+	private void positionsLeft(final boolean calculateTop) {
+		float top = 35;
+
+		for (int i = 0; i < groups.size(); i++) {
+			final Group group = groups.get(i);
+			if (calculateTop) {
+				top = group.calculatePosition(top);
+			}
+			group.setLeftSide(offsetLeft);
+		}
+	}
+
+	private void buildChildGroupsFromLeft() {
+		int nextID = groups.get(groups.size() - 1).getNumber() + 1;
+		int n = groups.size();
+		int tier = 1;
+		int parent = 0;
+		Group lastChild = groups.get(0);
+		List<Group> targetGroup = groups;
+		List<Group> childGroup = new LinkedList<Group>();
+
+		while (n > 1) {
+			offsetLeft += BOX_WIDTH + 30;
+			n = n / 2 + n % 2;
+			for (int i = 0; i < n; i++) {
+				final Group child = new Group(nextID + "_ _" + tier);
+				targetGroup.get(parent).setChildGroup(child);
+				addAdvanced(child, targetGroup.get(parent).getNames());
+				child.addParent(targetGroup.get(parent++));
+				if (targetGroup.size() > parent) {
+					targetGroup.get(parent).setChildGroup(child);
+					addAdvanced(child, targetGroup.get(parent).getNames());
+					child.addParent(targetGroup.get(parent++));
+				}
+				child.setLeftSide(offsetLeft);
+				fillGroup(child);
+				childGroup.add(child);
+				lastChild = child;
+				nextID++;
+			}
+			targetGroup = childGroup;
+			childGroup = new LinkedList<Group>();
+			parent = 0;
+			tier++;
+		}
+		if (n == 1) {
+			offsetLeft += BOX_WIDTH + 20;
+			finalBout = new Group(nextID + "_ _" + tier);
+			lastChild.setChildGroup(finalBout);
+			finalBout.addParent(lastChild);
+			addAdvanced(finalBout, lastChild.getNames());
+			finalBout.setLeftSide(offsetLeft);
+		}
+		fillGroup(finalBout);
+	}
+
+	private void buildChildGroups() {
+		int nextID = groups.get(groups.size() - 1).getNumber() + 1;
+
+		int n = onLeft;
+		int maxTier = 1;
+		int tier = 1;
+		int parent = 0;
+		Group lastChild = groups.get(0);
+		List<Group> targetGroup = groups;
+		List<Group> childGroup = new LinkedList<Group>();
+
+		while (n > 1) {
+			offsetLeft += BOX_WIDTH + 30;
+			n = n / 2 + n % 2;
+			for (int i = 0; i < n; i++) {
+				final Group child = new Group(nextID + "_ _" + tier);
+				targetGroup.get(parent).setChildGroup(child);
+				addAdvanced(child, targetGroup.get(parent).getNames());
+				child.addParent(targetGroup.get(parent++));
+				if (onLeft > parent) {
+					targetGroup.get(parent).setChildGroup(child);
+					addAdvanced(child, targetGroup.get(parent).getNames());
+					child.addParent(targetGroup.get(parent++));
+				}
+				child.setLeftSide(offsetLeft);
+				fillGroup(child);
+				childGroup.add(child);
+				lastChild = child;
+				nextID++;
+			}
+			targetGroup = childGroup;
+			childGroup = new LinkedList<Group>();
+			parent = 0;
+			tier++;
+		}
+		if (n == 1) {
+			offsetLeft += BOX_WIDTH + 20;
+			finalBout = new Group(nextID + "_ _" + tier);
+			lastChild.setChildGroup(finalBout);
+			finalBout.addParent(lastChild);
+			addAdvanced(finalBout, lastChild.getNames());
+			finalBout.setLeftSide(offsetLeft);
+		}
+
+		maxTier = tier;
+		int tiers = tier;
+		if (onRight == 2) {
+			tiers--;
+		}
+		offsetLeft += tiers * (BOX_WIDTH + 30);
+		offsetLeft -= 10;
+		n = onRight;
+		parent = onLeft;
+		tier = 1;
+		targetGroup = groups;
+		childGroup = new LinkedList<Group>();
+		for (int i = parent; i < groups.size(); i++) {
+			targetGroup.get(i).setLeftSide(offsetLeft);
+		}
+
+		while (n > 1) {
+			offsetLeft -= BOX_WIDTH + 30;
+			n = n / 2 + n % 2;
+			for (int i = 0; i < n; i++) {
+				final Group child = new Group(nextID + "_ _" + tier);
+				targetGroup.get(parent).setChildGroup(child);
+				addAdvanced(child, targetGroup.get(parent).getNames());
+				child.addParent(targetGroup.get(parent++));
+				if (targetGroup.size() > parent) {
+					targetGroup.get(parent).setChildGroup(child);
+					addAdvanced(child, targetGroup.get(parent).getNames());
+					child.addParent(targetGroup.get(parent++));
+				}
+				child.setLeftSide(offsetLeft);
+				fillGroup(child);
+				childGroup.add(child);
+				lastChild = child;
+				nextID++;
+			}
+			targetGroup = childGroup;
+			childGroup = new LinkedList<Group>();
+			parent = 0;
+			tier++;
+		}
+		// if right side has less groups than left side.
+		if (maxTier != tier) {
+			// for (VGroup parentGroup : finalBout.getParents()) {
+			if (lastChild.getLeftSide() > finalBout.getLeftSide()) {
+				for (final Competitor p : lastChild.getNames()) {
+					if (p.advancedTo() == tier) {
+						p.advancedToTier++;
+					} else if (p.advancedTo() == tier + 1) {
+						p.advancedToTier += 2;
+					}
+				}
+			}
+		}
+		// }
+		if (n == 1 || onRight == 1) {
+			if (onRight == 1) {
+				final Group parentGroup = groups.get(parent);
+				parentGroup.setChildGroup(finalBout);
+				finalBout.addParent(parentGroup);
+				addAdvanced(finalBout, parentGroup.getNames());
+			} else {
+				lastChild.setChildGroup(finalBout);
+				finalBout.addParent(lastChild);
+				addAdvanced(finalBout, lastChild.getNames());
+			}
+		}
+		if (hasAdvance(finalBout.getNames(), maxTier + 1)) {
+			winner = new Group("99_ _" + (maxTier + 1));
+			addAdvanced(winner, finalBout.getNames());
+		}
+		fillGroup(finalBout);
+	}
+
+	private void calculateChildPositions() {
+		final List<Group> allGroups = new LinkedList<Group>();
+
+		for (final Group group : groups) {
+			Group child = group;
+			while (child.getChildGroup() != null) {
+				child = child.getChildGroup();
+				if (!allGroups.contains(child)) {
+					allGroups.add(child);
+				}
+			}
+		}
+
+		while (true) {
+			if (calculatePosition(allGroups)) {
+				break;
+			}
+		}
+
+	}
+
+	private boolean calculatePosition(final List<Group> allGroups) {
+		final List<Group> remove = new ArrayList<Group>();
+
+		for (final Group group : allGroups) {
+			boolean allParentsHavePosition = true;
+			for (final Group parent : group.getParents()) {
+				if (!parent.hasPosition()) {
+					allParentsHavePosition = false;
+				}
+			}
+			if (allParentsHavePosition) {
+				float bottom = Float.MAX_VALUE;
+				float top = 0;
+				for (final Group parent : group.getParents()) {
+					if (parent.getBottom() < bottom) {
+						bottom = parent.getBottom();
+					}
+					if (parent.getTop() > top) {
+						top = parent.getTop();
+					}
+				}
+				group.calculatePositionFromMiddle(bottom + (top - bottom) / 2);
+				if (winner != null && group == finalBout) {
+					winner.calculatePosition(group.getTop() - 40);
+					winner.setLeftSide(group.getLeftSide());
+					remove.add(winner);
+				}
+				remove.add(group);
+			}
+		}
+
+		allGroups.removeAll(remove);
+
+		return allGroups.isEmpty();
+	}
+
+	private void drawChart() {
+		drawnGroups.clear();
+
+		canvas.getContext2d().clearRect(0.0, 0.0, canvas.getCoordinateSpaceWidth(), canvas.getCoordinateSpaceWidth());
+
+		for (int j = 0; j < onLeft; j++) {
+			final Group group = groups.get(j);
+			paint.left(group);
+			drawnGroups.add(group);
+			drawChild(group.getChildGroup());
+		}
+		drawParent(finalBout);
+	}
+
+	private void drawLeftChart() {
+		drawnGroups.clear();
+
+		canvas.getContext2d().clearRect(0.0, 0.0, canvas.getCoordinateSpaceWidth(), canvas.getCoordinateSpaceWidth());
+
+		for (final Group group : groups) {
+			paint.left(group);
+			drawnGroups.add(group);
+			if (group.getChildGroup() != null) {
+				drawChild(group.getChildGroup());
+			}
+		}
+	}
+
+	Map<Integer, Boolean> drawTier = new HashMap<Integer, Boolean>();
+
+	private void drawChild(final Group childGroup) {
+		if (drawnGroups.contains(childGroup)) {
+			return;
+		}
+
+		if (childGroup == finalBout) {
+			paint.finalBout(finalBout);
+			if (winner != null) {
+				paint.winner(winner, finalBout);
+				drawnGroups.add(winner);
+			}
+		} else {
+			paint.left(childGroup);
+		}
+		drawnGroups.add(childGroup);
+		if (childGroup.getChildGroup() != null) {
+			drawChild(childGroup.getChildGroup());
+		}
+	}
+
+	private void drawParent(final Group childGroup) {
+		offsetLeft += 30;
+		for (final Group parent : childGroup.getParents()) {
+
+			if (!drawnGroups.contains(parent)) {
+				if (childGroup.getParents().size() == 2 && childGroup != finalBout) {
+					paint.right(parent);
+				} else {
+					paint.right(parent);
+				}
+				if (parent.getParents() != null) {
+					offsetLeft += 100;
+					drawParent(parent);
+					offsetLeft -= 130;
+				}
+				drawnGroups.add(parent);
+			}
+		}
+	}
+
+	public static boolean hasAdvance(final List<Competitor> persons, final int toTier) {
+		for (final Competitor p : persons) {
+			if (p.advancedTo() >= toTier) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	private void addAdvanced(final Group child, final List<Competitor> persons) {
+		for (final Competitor p : persons) {
+			if (p.advancedTo() >= child.getTier()) {
+				child.addName(p);
+			}
+		}
+	}
+
+	private void fillGroup(final Group group) {
+		if (group.getNames().isEmpty()) {
+			group.addName(new Competitor(-1, "", 0));
+		}
+		if (group.getNames().size() == 1 && group != finalBout && group.getParents().size() > 1) {
+			group.addName(new Competitor(-1, "", 0));
+		}
+	}
+
+	/**
+	 * Sets the canvas width
+	 * 
+	 * @param width
+	 *            The width in pixels
+	 */
+	public void setCanvasWidth(final int width) {
+		if (width != this.width) {
+			VConsole.log("Setting width: " + width + "px");
+			canvas.setCoordinateSpaceWidth(width);
+			VConsole.log("Canvas width: " + canvas.getCoordinateSpaceWidth());
+		}
+	}
+
+	/**
+	 * Sets the canvas height
+	 * 
+	 * @param height
+	 *            The height in pixels
+	 */
+	public void setCanvasHeight(final int height) {
+		if (height != this.height) {
+			VConsole.log("Setting height: " + height + "px");
+			canvas.setCoordinateSpaceHeight(height);
+			VConsole.log("Canvas height: " + canvas.getCoordinateSpaceHeight());
+		}
+	}
+
+	public int getWidgetWidth() {
+		if (pixelWidth != null) {
+			return pixelWidth;
+		}
+		try {
+			final int width = Integer.parseInt(DOM.getAttribute(getElement(), "width").replaceAll("px", ""));
+			return width;
+		} catch (final Exception e) {
+			try {
+				final int width = Integer.parseInt(DOM.getStyleAttribute(getElement(), "width").replaceAll("px", ""));
+				return width;
+			} catch (final Exception f) {
+				return getOffsetWidth();
+			}
+		}
+	}
+
+	float xDown = 0, yDown = 0;
+
+	@Override
+	public void onBrowserEvent(final Event event) {
+		if (!enableDragging) {
+			return;
+		}
+
+		switch (DOM.eventGetType(event)) {
+		case Event.ONMOUSEDOWN:
+			mouseDown = true;
+			xDown = event.getClientX();
+			yDown = event.getClientY();
+			Event.setCapture(getElement());
+			break;
+		case Event.ONMOUSEMOVE:
+			if (!mouseDown) {
+				break;
+			}
+			mouseMoved = true;
+			float xChange = event.getClientX() - xDown;
+			float yChange = event.getClientY() - yDown;
+
+			xDown = event.getClientX();
+			yDown = event.getClientY();
+			for (final Group group : drawnGroups) {
+				group.updatePosition(yChange);
+				group.setLeftSide(group.getLeftSide() + xChange);
+			}
+			if (onlyLeft) {
+				drawLeftChart();
+			} else {
+				drawChart();
+			}
+			break;
+		case Event.ONMOUSEUP:
+			if (!mouseMoved) {
+				mouseMoved = false;
+				mouseDown = false;
+				final int x = event.getClientX() - getAbsoluteLeft();
+				final int y = event.getClientY() - getAbsoluteTop();
+				for (final Group group : drawnGroups) {
+					if (group.getLeftSide() < x && x < group.getLeftSide() + BOX_WIDTH && group.getTop() < y && y < group.getBottom()) {
+						try {
+							final Competitor person = group.getNames().get((int) (y - group.getTop()) / 20);
+							valueSelect(person.getId());
+						} catch (final IndexOutOfBoundsException ioob) {
+							Window.alert("Problem finding person");
+							// Failed to find user
+						}
+						break;
+					}
+				}
+			}
+			Event.releaseCapture(getElement());
+			mouseMoved = false;
+			mouseDown = false;
+			break;
+		case Event.ONTOUCHSTART:
+			Touch touch = event.getTargetTouches().get(0);
+			xDown = touch.getPageX();
+			yDown = touch.getPageY();
+			break;
+		case Event.ONTOUCHMOVE:
+			mouseMoved = true;
+
+			touch = event.getTargetTouches().get(0);
+			xChange = touch.getPageX() - xDown;
+			yChange = touch.getPageY() - yDown;
+
+			xDown = touch.getPageX();
+			yDown = touch.getPageY();
+
+			for (final Group group : drawnGroups) {
+				group.updatePosition(yChange);
+				group.setLeftSide(group.getLeftSide() + xChange);
+			}
+
+			if (onlyLeft) {
+				drawLeftChart();
+			} else {
+				drawChart();
+			}
+			break;
+		case Event.ONTOUCHCANCEL:
+		case Event.ONTOUCHEND:
+			mouseMoved = false;
+			break;
+		}
+	}
+
+	public void valueSelect(final long value) {
+		fireEvent(new SelectionEvent(value));
+	}
+
+	public HandlerRegistration addSelectionEventHandler(final SelectionEventHandler selectionEventHandler) {
+		return addHandler(selectionEventHandler, SelectionEvent.getType());
+	}
+
+}
---
+++ Competition-chart-demo/src/main/java/com/competition/chart/visualisation/demo/DemoUI.java
@@ -0,0 +1,188 @@
+package com.competition.chart.visualisation.demo;
+
+import javax.servlet.annotation.WebServlet;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Random;
+
+import com.competition.chart.visualisation.KnockoutChart;
+import com.competition.chart.visualisation.client.Competitor;
+
+import com.vaadin.annotations.Theme;
+import com.vaadin.annotations.Title;
+import com.vaadin.annotations.VaadinServletConfiguration;
+import com.vaadin.server.VaadinRequest;
+import com.vaadin.server.VaadinServlet;
+import com.vaadin.ui.Button;
+import com.vaadin.ui.CheckBox;
+import com.vaadin.ui.HorizontalLayout;
+import com.vaadin.ui.UI;
+import com.vaadin.ui.VerticalLayout;
+
+@Theme("demo")
+@Title("MyComponent Add-on Demo")
+@SuppressWarnings("serial")
+public class DemoUI extends UI {
+
+    private final Map<Integer, Competitor> participants = new HashMap<Integer, Competitor>();
+    private KnockoutChart knockoutChart;
+    private final Random rand = new Random(System.currentTimeMillis());
+
+    VerticalLayout content = new VerticalLayout();
+
+    @WebServlet(value = "/*", asyncSupported = true)
+    @VaadinServletConfiguration(productionMode = false, ui = DemoUI.class)
+    public static class Servlet extends VaadinServlet {
+    }
+
+    @Override
+    protected void init(VaadinRequest request) {
+
+        getPage().setTitle("Chart Application");
+        setContent(content);
+
+        participants.put(1, new Competitor(1, getName(), 1));
+        participants.put(2, new Competitor(2, getName(), 0));
+        participants.put(3, new Competitor(3, getName(), 0));
+        participants.put(4, new Competitor(4, getName(), 0));
+        participants.put(5, new Competitor(5, getName(), 0));
+        participants.put(6, new Competitor(6, getName(), 1));
+        participants.put(7, new Competitor(7, getName(), 1));
+        participants.put(8, new Competitor(8, getName(), 0));
+        participants.put(9, new Competitor(9, getName(), 0));
+        participants.put(10, new Competitor(10, getName(), 0));
+        participants.put(11, new Competitor(11, getName(), 0));
+        participants.put(12, new Competitor(12, getName(), 1));
+        participants.put(13, new Competitor(13, getName(), 0));
+        participants.put(14, new Competitor(14, getName(), 0));
+        participants.put(15, new Competitor(15, getName(), 0));
+        participants.put(16, new Competitor(16, getName(), 0));
+        participants.put(17, new Competitor(17, getName(), 0));
+        participants.put(18, new Competitor(18, getName(), 0));
+        participants.put(19, new Competitor(19, getName(), 0));
+        participants.put(20, new Competitor(20, getName(), 0));
+
+        final List<Competitor> group1 = new LinkedList<Competitor>();
+        group1.add(participants.get(1));
+
+        final List<Competitor> group2 = new LinkedList<Competitor>();
+        group2.add(participants.get(2));
+        group2.add(participants.get(3));
+
+        final List<Competitor> group3 = new LinkedList<Competitor>();
+        group3.add(participants.get(4));
+        group3.add(participants.get(5));
+        group3.add(participants.get(12));
+
+        final List<Competitor> group4 = new LinkedList<Competitor>();
+        group4.add(participants.get(6));
+
+        final List<Competitor> group5 = new LinkedList<Competitor>();
+        group5.add(participants.get(7));
+
+        final List<Competitor> group6 = new LinkedList<Competitor>();
+        group6.add(participants.get(8));
+        group6.add(participants.get(9));
+
+        knockoutChart = new KnockoutChart();
+        knockoutChart.addGroup(1, "A sarja", group1);
+        knockoutChart.addGroup(2, "B sarja", group2);
+        knockoutChart.addGroup(3, "C sarja", group3);
+        knockoutChart.addGroup(4, "D sarja", group4);
+        knockoutChart.addGroup(5, "E sarja", group5);
+        knockoutChart.addGroup(6, "F sarja", group6);
+        knockoutChart.addGroup(7, "G sarja", participants.get(10),
+                participants.get(11));
+        knockoutChart.addGroup(8, "H sarja", participants.get(13),
+                participants.get(14));
+        // knockoutChart.addGroup(9, "I sarja", participants.get(15),
+        // participants.get(16));
+        knockoutChart.setPanningEnabled(true);
+        knockoutChart.addListener(vcl);
+
+        final CheckBox left = new CheckBox("all on left");
+        left.addListener(event -> {
+            if (left.getValue()) {
+                knockoutChart.setChartMode(
+                        KnockoutChart.VisualisationMode.LEFT_ONLY);
+            } else {
+                knockoutChart.setChartMode(
+                        KnockoutChart.VisualisationMode.LEFT_RIGHT);
+            }
+            knockoutChart.requestRepaint();
+        });
+        // new ValueChangeListener() {
+        // private static final long serialVersionUID = -347103891605131202L;
+        //
+        // @Override
+        // public void valueChange(final ValueChangeEvent event) {
+        // if (left.booleanValue()) {
+        // knockoutChart.setChartMode(KnockoutChart.VisualisationMode.LEFT_ONLY);
+        // } else {
+        // knockoutChart.setChartMode(KnockoutChart.VisualisationMode.LEFT_RIGHT);
+        // }
+        // knockoutChart.requestRepaint();
+        // }
+        // });
+        final CheckBox pan = new CheckBox("Panning enabled");
+        pan.setValue(true);
+        // pan.setImmediate(true);
+        pan.addListener(event -> {
+            knockoutChart.setPanningEnabled(pan.getValue());
+            knockoutChart.requestRepaint();
+        });
+        // new ValueChangeListener() {
+        // private static final long serialVersionUID = -347103891605131202L;
+        //
+        // @Override
+        // public void valueChange(final ValueChangeEvent event) {
+        // knockoutChart.setPanningEnabled(pan.booleanValue());
+        // knockoutChart.requestRepaint();
+        // }
+        // });
+        final Button reset = new Button("Reset position",
+                new Button.ClickListener() {
+                    private static final long serialVersionUID = -4090369163773691780L;
+
+                    @Override
+                    public void buttonClick(final Button.ClickEvent event) {
+                        knockoutChart.resetChartPositions();
+                        knockoutChart.requestRepaint();
+                    }
+                });
+
+        final HorizontalLayout controlls = new HorizontalLayout();
+
+        controlls.addComponent(left);
+        controlls.addComponent(pan);
+        controlls.addComponent(reset);
+
+        content.addComponent(controlls);
+        content.addComponent(knockoutChart);
+
+        // setMainWindow(mainWindow);
+    }
+
+    private String getName() {
+        final StringBuilder name = new StringBuilder();
+        name.append(Names.surNames[rand.nextInt(Names.surNames.length)]);
+        name.append(" ");
+        name.append(Names.firstNames[rand.nextInt(Names.firstNames.length)]);
+
+        return name.toString();
+    }
+
+    private final KnockoutChart.ValueSelectListener vcl = new KnockoutChart.ValueSelectListener() {
+        private static final long serialVersionUID = 1835799531600547425L;
+
+        @Override
+        public void valueSelect(final KnockoutChart.ValueSelectEvent event) {
+            final Competitor c = participants
+                    .get(Integer.parseInt(Long.toString(event.getKey())));
+            c.addAdvanced();
+            knockoutChart.requestRepaint();
+        }
+    };
+}
